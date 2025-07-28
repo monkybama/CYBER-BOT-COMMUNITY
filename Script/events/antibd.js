eea058050e4a03b4ab42eb047a947fe6a2813592
saia632 <Saia.chan100@gmail.com>
2025-07-28 18:12:34 +0200

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - يا ماعز مجنون، لا يمكنك تغيير لقبك. 😹\n فقط رئيسي هو الذي يستطيع تغيير اللقب.🖐`}, threadID);
     }  
         }
