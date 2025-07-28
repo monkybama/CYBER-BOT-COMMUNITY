244b494881c08c2facc615e853c6a2013dd66bfb
saia632 <Saia.chan100@gmail.com>
2025-07-28 18:01:18 +0200

    Update god.js

--- Script/events/god.js
+++ Script/events/god.js
@@ -12,7 +12,7 @@ module.exports.config = {
 module.exports.run = async function({ api, event, Threads }) {
     const logger = require("../../utils/log");
     if (!global.configModule[this.config.name].enable) return;
-    var formReport =  "=== 𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 Notification ===" +
+    var formReport =  "=== 🪙✨𝑽𝑬𝑹𝑮𝑰𝑳 | 𝑮𝑶𝑳𝑫 𝐁𝐎𝐓 |🪙✨ - Notification ===" +
                         "\n\n» Thread mang ID: " + event.threadID +
                         "\n» Action: {task}" +
                         "\n» Action created by userID: " + event.author +
@@ -20,18 +20,18 @@ module.exports.run = async function({ api, event, Threads }) {
         task = "";
     switch (event.logMessageType) {
         case "log:thread-name": {
-            const oldName = (await Threads.getData(event.threadID)).name || "Name does not exist",
-                    newName = event.logMessageData.name || "Name does not exist";
+            const oldName = (await Threads.getData(event.threadID)).name || "الاسم غير موجود",
+                    newName = event.logMessageData.name || "الاسم غير موجود";
             task = "User changes group name from: '" + oldName + "' to '" + newName + "'";
             await Threads.setData(event.threadID, {name: newName});
             break;
         }
         case "log:subscribe": {
-            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "The user added the bot to a new group!";
+            if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) task = "أضاف المستخدم البوت إلى مجموعة جديدة!";
             break;
         }
         case "log:unsubscribe": {
-            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "The user kicked the bot out of the group!"
+            if (event.logMessageData.leftParticipantFbId== api.getCurrentUserID()) task = "قام المستخدم بطرد البوت من المجموعة!"
             break;
         }
         default: 
@@ -42,7 +42,7 @@ module.exports.run = async function({ api, event, Threads }) {
 
     formReport = formReport
     .replace(/\{task}/g, task);
-  var god = "100086680386976";
+  var god = "61562119538523";
 
     return api.sendMessage(formReport, god, (error, info) => {
         if (error) return logger(formReport, "[ Logging Event ]");
