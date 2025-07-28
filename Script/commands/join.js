4fdce1681ff439b3a227c0de5398ea82e2c1cade
saia632 <Saia.chan100@gmail.com>
2025-07-28 17:04:29 +0200

    Update join.js

--- Script/commands/join.js
+++ Script/commands/join.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 module.exports.config = {
-    name: "join",
+    name: "ضيفني",
     version: "1.0.1",
     hasPermssion: 2,
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
@@ -16,17 +16,17 @@ module.exports.handleReply = async function({ api, event, handleReply, Threads }
   var { threadID, messageID, senderID, body } = event;
   var { ID } = handleReply;
   console.log(ID)
-  if (!body || !parseInt(body)) return api.sendMessage('Your selection must be a number.', threadID, messageID);
-  if ((parseInt(body) - 1) > ID.length) return api.sendMessage("Your pick is not on the list", threadID, messageID);
+  if (!body || !parseInt(body)) return api.sendMessage('يجب أن يكون اختيارك رقمًا.', threadID, messageID);
+  if ((parseInt(body) - 1) > ID.length) return api.sendMessage("اختيارك ليس موجودا في القائمة", threadID, messageID);
   try {
     var threadInfo = await Threads.getInfo(ID[body - 1]);
     var { participantIDs, approvalMode, adminIDs } = threadInfo;
     if (participantIDs.includes(senderID)) return api.sendMessage(`You are already in this group.`, threadID, messageID);
     api.addUserToGroup(senderID, ID[body - 1]);
     if (approvalMode == true && !adminIDs.some(item => item.id) == api.getCurrentUserID()) return api.sendMessage("Added you to the group's approval list...Custom yourself.", threadID, messageID);
-    else return api.sendMessage(`My wife just added you to the group ${threadInfo.threadName} already. Check in the waiting or spam message section if you don't see the box \nA lot 💟`, threadID, messageID);
+    else return api.sendMessage(`لقد أضفتك للتو إلى المجموعة ${threadInfo.threadName} بالفعل. تحقق من قسم الرسائل المنتظرة أو غير المرغوب فيها إذا لم تجد المجموعة \nA lot 💟`, threadID, messageID);
   } catch (error) {
-    return api.sendMessage(`I'm wrong so I can't add you to that group:<.\n\n${error}`, threadID, messageID);
+    return api.sendMessage(`خطاء لا يمكنني إضافتك إلى هذه المجموعة:<.\n\n${error}`, threadID, messageID);
   }
 }
 
@@ -39,7 +39,7 @@ module.exports.run = async function({ api, event, Threads }) {
     msg += `${number}. ${i.threadInfo.threadName}\n`;
     ID.push(i.threadID)
   }
-  msg += `\n👉 Reply this message with the number corresponding to the group you want to enter`
+  msg += `\n👉 قم بالرد على هذه الرسالة بالرقم المقابل للمجموعة التي تريد الدخول إليها`
   return api.sendMessage(msg, threadID, (error, info) => {
     global.client.handleReply.push({
       name: this.config.name,
