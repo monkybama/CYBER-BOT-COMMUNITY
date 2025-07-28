8794ad69bbcbb2271bc8e23e494e52eba2746ec8
saia632 <Saia.chan100@gmail.com>
2025-07-28 15:21:52 +0200

    Update enrile.js

--- Script/commands/enrile.js
+++ Script/commands/enrile.js
@@ -1,6 +1,6 @@
 module.exports.config = {
     
-	name: "enrile",
+	name: "اينرال",
 	
 	version: "1.0.1",
 	hasPermssion: 0,
@@ -52,7 +52,7 @@ module.exports.run = async function({ api, event, args }) {
 	const axios = global.nodemodule["axios"];
 	let pathImg = __dirname + '/cache/enrile.png';
 	var text = args.join(" ");
-	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
+	if (!text) return api.sendMessage("أدخل محتوى التعليق على اللوحة", threadID, messageID);
 	let getPorn = (await axios.get(`https://i.imgur.com/1plDf6o.png`, { responseType: 'arraybuffer' })).data;
 	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
 	let baseImage = await loadImage(pathImg);
