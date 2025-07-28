2e315e7368eee8eee176329a5e5e0169cb00a6ed
saia632 <Saia.chan100@gmail.com>
2025-07-28 12:43:28 +0200

    Update clearcache.js

--- Script/commands/clearcache.js
+++ Script/commands/clearcache.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-	name: "clearcache",
+	name: "شال",
 	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
@@ -11,7 +11,7 @@ module.exports.config = {
 
 module.exports.run = async function ({ event, api, Currencies, args, Threads }) {
 const { writeFileSync, readdirSync, existsSync, unlinkSync } = require('fs-extra');
-const permission = ["100086680386976"];
+const permission = ["61562119538523"];
              if (!permission.includes(event.senderID))
              return api.sendMessage("ULLASH only.", event.threadID, event.messageID);
   /*
@@ -24,7 +24,7 @@ if (!existsSync(path)) writeFileSync(path, "tdungdeptrai", "utf-8");
 }
   }
   */
-  if(!args[0]){ return api.sendMessage('You Have Not Entered the FIle Extension Needed to be Deleted', event.threadID, event.messageID)}
+  if(!args[0]){ return api.sendMessage('لم تقم بإدخال امتداد الملف المطلوب حذفه', event.threadID, event.messageID)}
    const listFile = readdirSync(__dirname + '/cache').filter(item => item.endsWith("." + args[0]));
   var msg = "";
   for(i in listFile){
@@ -32,7 +32,7 @@ if (!existsSync(path)) writeFileSync(path, "tdungdeptrai", "utf-8");
     msg += `${listFile[i]}\n`
   }
   console.log(msg)
-  return api.sendMessage(`${msg}\n\nPlease Press Y to Delete the following files`, event.threadID, (error, info) =>{
+  return api.sendMessage(`${msg}\n\n الرجاء الضغط على Y لحذف الملفات التالية`, event.threadID, (error, info) =>{
     if(error) console.log(error)
     global.client.handleReply.push({
         step: 0,
