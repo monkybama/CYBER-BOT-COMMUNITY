448ed19b7e6a3df7c44c1fb7d7037056318a87c5
saia632 <Saia.chan100@gmail.com>
2025-07-28 09:31:40 +0200

    Offbot.js

--- Script/commands/Offbot.js
+++ Script/commands/Offbot.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-	name: "offbot",
+	name: "ايقاف",
 	version: "1.0.0",
 	hasPermssion: 2,
 	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100000478146113", "61576357565998"];
+    const permission = ["100001189452344", "61562119538523"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For ULLASH", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
