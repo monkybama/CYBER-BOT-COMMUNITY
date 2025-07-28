8321a36c475c0c760d112543d600a62e7b30ece5
saia632 <Saia.chan100@gmail.com>
2025-07-28 15:35:16 +0200

    Update fbget.js

--- Script/commands/fbget.js
+++ Script/commands/fbget.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "fbget",
+    name: "تنزيل",
     version: "1.0.0",
     hasPermssion: 0,
     credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
@@ -12,21 +12,21 @@ module.exports.run = async function ({api,event,args})  {
 const axios = global.nodemodule['axios'];  
 const fs = global.nodemodule["fs-extra"];
 try { 
-  if(args[0] == 'audio'){
-        api.sendMessage(`Processing request!!!`, event.threadID, (err, info) => 
+  if(args[0] == 'صوت'){
+        api.sendMessage(`طلب المعالجة!!!`, event.threadID, (err, info) => 
             
     setTimeout(() => {
         api.unsendMessage(info.messageID) } , 20000),event.messageID);
         const path = __dirname+`/cache/2.mp3`;
  let getPorn = (await axios.get(event.attachments[0].playableUrl,{ responseType:'arraybuffer'} )).data;
   fs.writeFileSync(path, Buffer.from(getPorn, "utf-8"));
-return api.sendMessage({body : `Loaded success🥱`, 
+return api.sendMessage({body : `نجاح تحميل 🥱`, 
     attachment: fs.createReadStream(path)}, event.threadID, () => fs.unlinkSync(path),event.messageID);
     }; 
-  }catch {return api.sendMessage(`Unable to process the request`,event.threadID,event.messageID)}
+  }catch {return api.sendMessage(`غير قادر على معالجة الطلب`,event.threadID,event.messageID)}
     try { 
-      if(args[0] == 'video'){
-            api.sendMessage(`Processing request!!!`, event.threadID, (err, info) =>
+      if(args[0] == 'فيديو'){
+            api.sendMessage(`طلب المعالجة!!!`, event.threadID, (err, info) =>
 
 
     setTimeout(() => {
@@ -34,8 +34,8 @@ return api.sendMessage({body : `Loaded success🥱`,
             const path1 = __dirname+`/cache/1.mp4`;
  let getPorn = (await axios.get(event.attachments[0].playableUrl,{ responseType:'arraybuffer'} )).data;
   fs.writeFileSync(path1, Buffer.from(getPorn, "utf-8"));
-return api.sendMessage({body : `Loaded success🥱`, 
+return api.sendMessage({body : `نجاح تحميل 🥱`, 
     attachment: fs.createReadStream(path1)}, event.threadID, () => fs.unlinkSync(path1),event.messageID);
     }; 
-  }catch {return api.sendMessage(`Unable to process the request`,event.threadID,event.messageID)}
+  }catch {return api.sendMessage(`غير قادر على معالجة الطلب`,event.threadID,event.messageID)}
 }
